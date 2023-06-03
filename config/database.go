bed5d56fda06203dff1787f918ef42bbe96d7ce1
eicheich <nengindah41@gmail.com>
2023-06-04 05:13:02 +0700

    [HK] set up databse

---
+++ config/database.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func ConnnectDB() {
+
+	db, err := sql.Open("mysql", "root:@/b_gocrud")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Connected to database")
+	DB = db
+}
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"go-crud/config"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config.ConnnectDB()
+
 	log.Println("iyyah")
 	http.ListenAndServe(":8080", nil)
 }
