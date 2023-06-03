556bdf1061a296ccf70e80d6d12d0ced93360ced
eicheich <nengindah41@gmail.com>
2023-06-04 05:22:44 +0700

    [[HK] yeyy it works

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"go-crud/config"
+	homecontroller "go-crud/controllers"
 	"log"
 	"net/http"
 )
 
 func main() {
 	config.ConnnectDB()
+
+	http.HandleFunc("/", homecontroller.Welcome)
 
 	log.Println("iyyah")
 	http.ListenAndServe(":8080", nil)
